f060e459328cf561bcbd33e8dee1bf177d4fbb31
jiyoon0923 <dbadl0923@naver.com>
2022-02-28 00:32:51 +0900

    기존+점수판+점수판효과

--- app/src/main/java/com/example/sun_moon/exercise_screen.java
+++ app/src/main/java/com/example/sun_moon/exercise_screen.java
@@ -5,6 +5,8 @@ import static java.lang.Thread.sleep;
 import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
@@ -26,6 +28,7 @@ public class exercise_screen extends AppCompatActivity {
     ImageView image;
     private int progressStatus = 0;
     private int timerStatus = 90;
+    private int score_text = 0;
 
     int move_num = 1000;
 
@@ -47,7 +50,8 @@ public class exercise_screen extends AppCompatActivity {
         ImageButton btn = findViewById(R.id.btn); // 호랑이
         TextView tv = findViewById(R.id.tv);
         ProgressBar pb = findViewById(R.id.pb);
-        TextView time = findViewById(R.id.time);
+        Button time = findViewById(R.id.time);
+        Button score = findViewById(R.id.score);
 
         Timer(time);
 
@@ -60,17 +64,17 @@ public class exercise_screen extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while(progressStatus < 100){
+                while(progressStatus < 100) {
                     // Update the progress status
 //                            if(운동하면){
 //                                잠시 멈추고 progressStatus 저장,
 //                                다시 안움직이면 다시 시작
 //                            }
-                    progressStatus +=1;
+                    progressStatus += 1;
                     // Try to sleep the thread for 20 milliseconds
-                    try{
+                    try {
                         sleep(300);
-                    }catch(InterruptedException e){
+                    } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                     // Update the progress
@@ -80,9 +84,9 @@ public class exercise_screen extends AppCompatActivity {
                         public void run() {
                             pb.setProgress(progressStatus);
                             // Show the progress on TextView
-                            tv.setText(progressStatus+"");
+                            tv.setText(progressStatus + "");
                             // If task execution completed
-                            if(progressStatus >= 80){
+                            if (progressStatus >= 80) {
                                 // Set a message of completion
                                 tv.setText("곧 호랑이 ~!");
                                 tv.setTextColor(0xAAef484a);
@@ -90,13 +94,14 @@ public class exercise_screen extends AppCompatActivity {
                                 //pb.setProgressBackgroundTintList(ColorStateList.valueOf(Color.RED));
                                 //호랑이 쪽에 100이라고 알림
                             }
-                            if(progressStatus == 100){
+                            if (progressStatus == 100) {
                                 // Set a message of completion
                                 tv.setText("어흥");
                                 //호랑이 쪽에 100이라고 알림
                             }
                         }
                     });
+
                 }
             }
         }).start(); // Start the operation
@@ -129,6 +134,7 @@ public class exercise_screen extends AppCompatActivity {
                     scroll.fullScroll(ScrollView.FOCUS_DOWN);
                     up.setY(image.getHeight()- view.getHeight());
                     time.setY(image.getHeight()- view.getHeight() + 50);
+                    score.setY(image.getHeight()- view.getHeight() + 50);
                     pb.setY(image.getHeight()- view.getHeight() + 600);
                     btn.setY(image.getHeight()- view.getHeight() + 1900);
                     tv.setY(image.getHeight()- view.getHeight() + 1500);
@@ -159,6 +165,10 @@ public class exercise_screen extends AppCompatActivity {
                         else{
                             progressStatus-=10;
                         }
+
+                        Score(score);
+
+
                         //scroll.smoothScrollBy(originX, originY - 1000);
 //                        ObjectAnimator.ofInt(scroll, "scrollY", view.getHeight()).setDuration(60000).start();
 //                        ObjectAnimator.ofFloat(up, "Y", view.getHeight()).setDuration(60000).start();
@@ -170,19 +180,25 @@ public class exercise_screen extends AppCompatActivity {
                         ObjectAnimator.ofInt(scroll, "scrollY", Math.round(up.getY()), Math.round(up.getY()-move_num)).setDuration(600).start();
                         ObjectAnimator.ofFloat(up, "Y", up.getY(), up.getY()-move_num).setDuration(600).start();
                         ObjectAnimator.ofFloat(time, "Y", time.getY(), time.getY()-move_num).setDuration(600).start();
+                        ObjectAnimator.ofFloat(score, "Y", time.getY(), time.getY()-move_num).setDuration(600).start();
                         ObjectAnimator.ofFloat(pb, "Y", pb.getY(), pb.getY()-move_num).setDuration(600).start();
                         ObjectAnimator.ofFloat(btn, "Y", btn.getY(), btn.getY()-move_num).setDuration(600).start();
                         ObjectAnimator.ofFloat(tv, "Y", tv.getY(), tv.getY()-move_num).setDuration(600).start();
 
-
-
+                        //score.setBackgroundResource(R.drawable.score);
                     }
+
                 });
+
+                        score.setBackgroundResource(R.drawable.score);
+
             }
         });
 
+
+
     }
-    public void Timer(TextView view) {
+    public void Timer(Button btn) {
         // Start the lengthy operation in a background thread
         new Thread(new Runnable() {
             @Override
@@ -207,25 +223,23 @@ public class exercise_screen extends AppCompatActivity {
                         public void run() {
                             if(timerStatus>=60){
                                 if((timerStatus-60)<=9){
-                                    view.setText("1:0"+(timerStatus-60));
+                                    btn.setText("1:0"+(timerStatus-60));
                                 }
                                 else {
-                                    view.setText("1:"+(timerStatus-60));
+                                    btn.setText("1:"+(timerStatus-60));
                                 }
                             }
                             else if((timerStatus<=9)){
-                                view.setText("0:0"+(timerStatus));
+                                btn.setText("0:0"+(timerStatus));
                             }
                             else{
-                                view.setText("0:"+(timerStatus));
+                                btn.setText("0:"+(timerStatus));
                             }
 
                             // If task execution completed
                             if(timerStatus <=30){
                                 // Set a message of completion
-
-
-                                view.setTextColor(0xAAef484a);
+                                btn.setTextColor(0xAAef484a);
                             }
 
                         }
@@ -235,6 +249,20 @@ public class exercise_screen extends AppCompatActivity {
         }).start(); // Start the operation
 
     }
+
+
+    public void Score(Button btn) {
+        score_text+=1;
+        btn.setText(String.valueOf(score_text));
+        btn.setBackgroundResource(R.drawable.score_plus);
+//        try{
+//            Thread.sleep(1000);
+//        }catch(InterruptedException e){
+//            e.printStackTrace();
+//        }
+//        btn.setBackgroundResource(R.drawable.score);
+    }
+
 }
 
 
