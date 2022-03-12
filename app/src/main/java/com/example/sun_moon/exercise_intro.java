791cbede389dee3d6e80e5ead45aa45e026a1168
naeun <tina1296@naver.com>
2022-03-12 20:30:51 +0900

    exercise_intro 화면 타이머 이후에 바로 다음 화면 전환

--- app/src/main/java/com/example/sun_moon/exercise_intro.java
+++ app/src/main/java/com/example/sun_moon/exercise_intro.java
@@ -6,8 +6,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.view.Surface;
-import android.view.SurfaceHolder;
+import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -16,22 +15,46 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.VideoView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class exercise_intro extends AppCompatActivity {
     Button skip, back;
-    TextView starttext;
+    TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercies_intro);
+        txt=findViewById(R.id.txt);
         final VideoView videoView=(VideoView) findViewById(R.id.video);
         MediaController mc=new MediaController(this);
         videoView.setMediaController(mc);
         videoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.im));
         videoView.start();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                CountDownTimer countDownTimer=new CountDownTimer(15000,1000) {
+                    @Override
+                    public void onTick(long millisUntilFinished) {
+                        int num=(int) (millisUntilFinished/1000);
+                        txt.setText(Integer.toString(num+1));
+
+                    }
+
+                    @Override
+                    public void onFinish() {
+                        txt.setText("시작");
+                        Intent start_intent = new Intent(exercise_intro.this, exercise_screen.class);
+                        startActivity(start_intent);
+
+                    }
+                }.start();
+
+            }
+        },6000);
+
 
 
 
@@ -56,6 +79,5 @@ public class exercise_intro extends AppCompatActivity {
         });
 
     }
-
 
 }
